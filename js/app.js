3be18f2f6f52482e3a5f9dd9669ac4f585ca55ea
evan <eulloyd@gmail.com>
2014-05-23 05:34:49 +0000

    Add a controller

---
+++ js/app.js
@@ -0,0 +1,12 @@
+angular.module('myApp', [])
+  .controller('mainController', function($scope){
+    $scope.maleName = "male name",
+    $scope.jobTitle = "job title",
+    $scope.tediousTask = "tedious task",
+    $scope.dirtyTask = "dirty task",
+    $scope.celebrity = "celebrity",
+    $scope.uselessSkill = "useless skill",
+    $scope.obnoxiousCelebrity = "obnoxious celebrity",
+    $scope.hugeNumber = "huge number",
+    $scope.adjective = "adjective"
+  });
