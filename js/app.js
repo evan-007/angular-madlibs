2993ef00dff041f2b56e985b29214af4ce367501
Evan Lloyd <eulloyd@gmail.com>
2014-05-23 23:11:02 +0900

    Refactor elements into data

--- js/app.js
+++ js/app.js
@@ -1,15 +1,22 @@
 angular.module('myApp', [])
   .controller('mainController', function($scope){
-    $scope.person = {
+    $scope.submit = function(){
+      if($scope.myform.$valid) {
+        console.log('form is valid ', $scope.data);
+      } else {
+        console.log('the form is invalid');
+      }
+    };
+    $scope.data = {
       name: "name",
-      gender: "male"
-    },
-    $scope.jobTitle = "job title",
-    $scope.tediousTask = "tedious task",
-    $scope.dirtyTask = "dirty task",
-    $scope.celebrity = "celebrity",
-    $scope.uselessSkill = "useless skill",
-    $scope.obnoxiousCelebrity = "obnoxious celebrity",
-    $scope.hugeNumber = "huge number",
-    $scope.adjective = "adjective"
+      gender: "male",
+      jobTitle: "job title",
+      tediousTask: "tedious task",
+      dirtyTask: "dirty task",
+      celebrity: "celebrity",
+      uselessSkill: "useless skill",
+      obnoxiousCelebrity: "obnoxious celebrity",
+      hugeNumber: "huge number",
+      adjective: "adjective"
+    };
   });
